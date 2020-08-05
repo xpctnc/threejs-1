33475063df52cf09bf50b240c8f5c3ee9d32ecd5
Adrian Prusik <xpctnc@gmail.com>
2020-08-05 16:29:34 +0200

    Change element to sphere

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer;
 
-const camera = new THREE.PerspectiveCamera(24, window.innerWidth / window.innerHeight, .1, 1000);
+scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, .1, 1000);
+
+renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+const colorLight = new THREE.Color('hsl(100, 100%, 100%)');
 const colorPink = new THREE.Color('hsl(306, 100%, 60%)');
 const colorYellow = new THREE.Color('hsl(50, 100%, 60%)');
-const colorLight = new THREE.Color('hsl(100, 100%, 100%)');
-
-const cubeGeometry = new THREE.BoxGeometry(2, 1.5, 1.2);
 
 const cubeMaterial = new THREE.MeshPhongMaterial({
     color: colorLight,
 })
 
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
 const light = new THREE.PointLight(colorPink, 2);
 const light2 = new THREE.PointLight(colorYellow, 2);
 
-light.position.set(-40, -20, 20);
-light2.position.set(40, 20, 15);
-
-scene.add(light);
-scene.add(light2);
-scene.add(cube);
-
 camera.position.z = 15;
 
-cube.rotation.x = 20;
-cube.rotation.z = -20;
+const handleResize = () => {
+    const { innerWidth, innerHeight } = window;
+    renderer.setSize(innerWidth, innerHeight);
+    camera.aspect = innerWidth / innerHeight;
+    camera.updateProjectionMatrix();
 
-const animate = () => {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.z += 0.01;
-    renderer.render(scene, camera);
 }
 
-animate();
+
+const createSphere = (s = 1, color = '0xFFFFFF') => {
+    const sphereGeometry = new THREE.SphereGeometry(s, 20, 20);
+    const sphereMaterial = new THREE.MeshPhongMaterial({
+        color,
+        shininess: 50,
+    })
+
+    return new THREE.Mesh(sphereGeometry, sphereMaterial);
+}
+
+const createPointLight = (i = 1, color = 0xFFFFFF) => new THREE.PointLight(color, i)
+
+const nucleus = createSphere(3);
+const l1 = createPointLight();
+const l2 = createPointLight();
+
+l1.position.set(30, 5 ,10)
+l2.position.set(60, 0, 20)
+
+scene.add(nucleus, l1, l2);
+
+const loop = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(loop)
+}
+
+loop();
+window.addEventListener('resize', handleResize);
